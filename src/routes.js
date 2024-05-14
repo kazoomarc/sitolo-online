92a8d42f24d39c1adf4e8e3bc37e24c6b6522b48
Joel Mwala <joelmwala7@gmail.com>
2024-05-14 06:35:56 +0100

    connected all nav routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,14 @@
+import Home from './components/home/home';
+import Marketplace from './components/marketplace/marketplace';
+import Login from './components/login/login';
+import Pricing from './components/pricing/pricing';
+import Products from './components/products/products';
+import Settings from './components/settings/settings';
+import Terminal from './components/terminal/terminal';
+import Signup from './components/signup/signup';
+import Dashboard from './components/dashboard/dashboard';
+import Team from './components/team/team';
+
 const { createBrowserRouter } = require('react-router-dom');
 
 const router = createBrowserRouter([
@@ -5,9 +16,46 @@ const router = createBrowserRouter([
     path: '/',
     element: <Home />,
   },
+
   {
-    path: '/',
+    path: '/home',
     element: <Home />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/marketplace',
+    element: <Marketplace />,
+  },
+  {
+    path: '/pricing',
+    element: <Pricing />,
+  },
+  {
+    path: '/products',
+    element: <Products />,
+  },
+  {
+    path: '/settings',
+    element: <Settings />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/team',
+    element: <Team />,
+  },
+  {
+    path: '/terminal',
+    element: <Terminal />,
   },
 ]);
 
