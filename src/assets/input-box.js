bd5a15c3dcd2d1c7cd290897aab9b11a0cbde1ce
Joel Mwala <joelmwala7@gmail.com>
2024-05-15 02:15:00 +0200

    done with inputbox

--- src/assets/input-box.js
+++ src/assets/input-box.js
@@ -3,7 +3,7 @@ export default function InputBox(props) {
     <input
       type={props.type}
       placeholder={props.text}
-      className="input-box rounded-md"
+      className="mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200"
       style={{ width: props.width + 'px' }}
       target={props.target}
     />
