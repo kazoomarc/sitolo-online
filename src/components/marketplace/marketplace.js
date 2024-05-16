4b4d72de01aaa32ad3c23fd5306d45f100c4a4b7
“probenjamin” <bed-com-02-21@unima.ac.mw>
2024-05-16 17:09:27 +0100

    add template card

--- src/components/marketplace/marketplace.js
+++ src/components/marketplace/marketplace.js
@@ -1,12 +1,18 @@
 // import { NavLink } from 'react-router-dom';
 import NavTop from '../../common-components/top-nav';
 import MainWrapper from '../../common-components/main-wrapper';
+import Card from '../../assets/card';
 
 export default function Marketplace() {
   return (
     <>
       <NavTop title="Marketplace" />
-      <MainWrapper></MainWrapper>
+      <MainWrapper>
+        <h1>Sitolo Online</h1>
+        <Card width="100%">
+          <h2>Stores available</h2>
+        </Card>
+      </MainWrapper>
     </>
   );
 }
