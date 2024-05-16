750477252e4ca5bbce079115f74eed1f0d278554
Joel Mwala <joelmwala7@gmail.com>
2024-05-16 16:46:27 +0200

    add type of input boxes

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,6 +1,8 @@
 // import { NavLink } from 'react-router-dom';
 import { Button } from '../../assets/button';
+import Card from '../../assets/card';
 import InputBox from '../../assets/input-box';
+import PearlButton from '../../assets/pearl-btn';
 import MainWrapper from '../../common-components/main-wrapper';
 import NavTop from '../../common-components/top-nav';
 
@@ -21,26 +23,26 @@ export function SignupForm() {
       <form action="/signup" className="max-w-[20rem]">
         <div className="mt-8">
           <label for="fname">Full Name</label>
-          <InputBox text="full name" />
+          <InputBox type="text" text="full name" />
         </div>
 
         <div className="mt-8">
           <label for="fname">Email</label>
-          <InputBox text="email address" />
+          <InputBox type="email" text="email address" />
         </div>
 
         <div className="mt-8">
           <label for="fname">Password</label>
-          <InputBox text="password" />
+          <InputBox type="password" text="password" />
         </div>
 
         <div className="mt-8">
           <label for="fname">Password</label>
-          <InputBox text="re-enter password" />
+          <InputBox type="password" text="re-enter password" />
         </div>
 
         <div className="mt-8">
-          <Button text="SIGN UP" width="100%" link="/signup" />
+          <Button type="submit" text="SIGN UP" width="100%" link="/signup" />
         </div>
       </form>
     </>
