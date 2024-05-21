2905898889e23adf3335833719401f0dbe06b543
Joel Mwala <joelmwala7@gmail.com>
2024-05-21 19:35:35 +0200

    to checkout

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,5 +1,5 @@
 // import { NavLink } from 'react-router-dom';
-import Button from'../../common-components/main-button';
+import Button from '../../common-components/main-button';
 import MainWrapper from '../../common-components/main-wrapper';
 import NavTop from '../../common-components/top-nav';
 export default function Dashboard() {
@@ -10,7 +10,7 @@ export default function Dashboard() {
         <h1>store summary</h1>
         <h2>Total amount amount products</h2>
         <span>SEE THE TOTAL NUMBER PRODUCTS AVAILABLE</span>
-        <Button text="see products" width = "100px" link="/dashboard"/>
+        <Button text="see products" width="100px" link="/dashboard" />
       </MainWrapper>
     </>
   );
--- src/components/terminal/terminal.js
+++ src/components/terminal/terminal.js
@@ -1,12 +1,17 @@
 // import { NavLink } from 'react-router-dom';
 import NavTop from '../../common-components/top-nav';
+import InputBox from '../../assets/input-box';
 import MainWrapper from '../../common-components/main-wrapper';
 
 export default function Terminal() {
   return (
     <>
       <NavTop title="Terminal" />
-      <MainWrapper></MainWrapper>
+      <MainWrapper>
+        <div className="search">
+          <InputBox text="search for product" />
+        </div>
+      </MainWrapper>
     </>
   );
 }
