4dc9b0df8bc5bbe2e0e68c4a813e502164d9b852
Joel Mwala <joelmwala7@gmail.com>
2024-05-21 19:53:41 +0200

    remove btn

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,17 +1,10 @@
-// import { NavLink } from 'react-router-dom';
-import Button from'../../common-components/main-button';
 import MainWrapper from '../../common-components/main-wrapper';
 import NavTop from '../../common-components/top-nav';
 export default function Dashboard() {
   return (
     <>
       <NavTop />
-      <MainWrapper>
-        <h1>store summary</h1>
-        <h2>Total amount amount products</h2>
-        <span>SEE THE TOTAL NUMBER PRODUCTS AVAILABLE</span>
-        <Button text="see products" width = "100px" link="/dashboard"/>
-      </MainWrapper>
+      <MainWrapper></MainWrapper>
     </>
   );
 }
