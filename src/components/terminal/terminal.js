a3512b3e80506e9ffff53f88d5a5c03419afb0f2
Joel Mwala <joelmwala7@gmail.com>
2024-05-21 22:39:14 +0200

    add product table and customer table

--- src/components/terminal/terminal.js
+++ src/components/terminal/terminal.js
@@ -17,7 +17,79 @@ export default function Terminal() {
             <Button text="search" />
           </div>
         </div>
+        <br />
+        <br />
+        <ProductChoiceTable />
+        <br />
+        <br />
+        <CustomerChoiceTable />
       </MainWrapper>
+    </>
+  );
+}
+
+function ProductChoiceTable() {
+  return (
+    <>
+      <div>
+        <table className="w-1/2 border-collapse border-slate-600 border">
+          <thead>
+            <tr>
+              <th className="w-1/3 p-2 border-slate-600 border text-left">
+                Product
+              </th>
+              <th className="w-1/3 p-2 border-slate-600 border text-left">
+                Price
+              </th>
+              <th className="w-1/3 p-2 border-slate-600 border text-left">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="w-1/3 p-2 border-slate-600 border">product</td>
+              <td className="w-1/3 p-2 border-slate-600 border">price</td>
+              <td className="w-1/3 p-2 border-slate-600 border">action</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+function CustomerChoiceTable() {
+  return (
+    <>
+      <div>
+        <table className="w-full border-collapse border-slate-600 border">
+          <thead>
+            <tr>
+              <th className="w-1/4 p-2 border-slate-600 border text-left">
+                Name of Product
+              </th>
+              <th className="w-1/4 p-2 border-slate-600 border text-left">
+                Quantity
+              </th>
+              <th className="w-1/4 p-2 border-slate-600 border text-left">
+                Price
+              </th>
+              <th className="w-1/4 p-2 border-slate-600 border text-left">
+                Amount
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="w-1/4 p-2 border-slate-600 border">product</td>
+              <td className="w-1/4 p-2 border-slate-600 border">price</td>
+              <td className="w-1/4 p-2 border-slate-600 border">action</td>
+              <td className="w-1/4 p-2 border-slate-600 border">action</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
